5267aa19aae832d59afd052b5cb33f1fbb4ab223
Anteneh Zeleke <antenehzeleke3@gmail.com>
2020-10-30 19:18:42 -0600

    for videep

--- client/for.OSP/src/main/java/RestfulServer.java
+++ client/for.OSP/src/main/java/RestfulServer.java
@@ -27,7 +27,7 @@ public class RestfulServer{
         return HttpRequestToJson;
         }
 
-        //return all the properties of the requests coming in
+        //return all the properties of the requests coming in.
         private String HttpRequestToJson(Request request){
         return "{\n"
                 +
