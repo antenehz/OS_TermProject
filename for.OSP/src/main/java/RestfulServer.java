6d0d0d19faf02fd0d88ff6d6e453e9a4229d8251
edgenicholson <edgenichollson2@gmail.com>
2020-11-01 15:47:03 -0700

    Added dependencies & properties to pom file

--- for.OSP/src/main/java/RestfulServer.java
+++ for.OSP/src/main/java/RestfulServer.java
@@ -1,6 +1,8 @@
+//import org.apache.log4j.BasicConfigurator;
 import spark.Spark;
 import spark.Request;
 import spark.Response;
+//import java.util.Properties;
 
 public class RestfulServer{
     public RestfulServer(){
@@ -10,31 +12,30 @@ public class RestfulServer{
 
         private void configureRestfulApiServer(){
         //Starting a Spark Microserver listening on the port specified
-        Spark.port(8080);
-        System.out.println("Server Configured to listen on port 8080");
+        Spark.port(5542);
+        System.out.println("Server Configured to listen on port 5542");
         }
         // configure Spark's API request routs
         private void  processRestfulAPIRequests(){
         //stub for the http get method and first
             Spark.get("/", this::echoRequest);
         }
-        // mathod that implements a spark rout
+        // method that implements a spark rout
         private String echoRequest(Request request, Response response){
         //set a response content type to application/json
-        response.type(contentType:"application/json");
-        response.head("Access-Control-Allow-Origin", "*");
+        response.type("application/json");
+        response.header("Access-Control-Allow-Origin", "*");
         response.status(200); // status is successful
-        return HttpRequestToJson;
+        return HttpRequestToJson(request);
         }
 
         //return all the properties of the requests coming in
         private String HttpRequestToJson(Request request){
-            System.out.println("Hello world");
-        System.out.println("Ew");
-
-        return "";
+            System.out.println(request.body());
+            return "{\n" + "\"body\":\"" + request.body() + "\", \n" + "}";
         }
         public static void main(String[] argv){
-        RestfulServer restfulServer = new RestfulServer();
+
+            RestfulServer restfulServer = new RestfulServer();
         }
         }
